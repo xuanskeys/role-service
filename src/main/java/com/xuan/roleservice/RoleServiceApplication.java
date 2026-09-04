b59ad094befdbd1886cf605e3741bf4c31e40361
xuan <xy@jxwxxkj.com>
2026-09-04 11:45:41 +0800

    nacos配置中心和服务注册发现中心接入

--- src/main/java/com/xuan/roleservice/RoleServiceApplication.java
+++ src/main/java/com/xuan/roleservice/RoleServiceApplication.java
@@ -3,9 +3,13 @@ package com.xuan.roleservice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @MapperScan("com.xuan.roleservice.mapper")
+@EnableDiscoveryClient
+@EnableFeignClients
 public class RoleServiceApplication {
 
     public static void main(String[] args) {
